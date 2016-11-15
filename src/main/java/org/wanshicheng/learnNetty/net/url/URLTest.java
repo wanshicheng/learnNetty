694f598efb86d03b3bb6fb62deb6a4f61db4fff6
Timothy Wan <wanshicheng@qq.com>
2016-11-15 14:55:38 +0800

    Initial commit

---
+++ src/main/java/org/wanshicheng/learnNetty/net/url/URLTest.java
@@ -0,0 +1,25 @@
+package org.wanshicheng.learnNetty.net.url;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * @author Timothy Wan
+ */
+public class URLTest {
+    public static void main(String[] args) {
+        try {
+            URL url = new URL("http://www.wanshicheng.org");
+            InputStream is = url.openStream();
+            int c;
+            while ((c = is.read()) != -1) System.out.write(c);
+            is.close();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
