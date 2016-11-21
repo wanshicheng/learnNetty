dc1aed7f0744ea976ac696fee9821873b2eef408
Timothy Wan <wanshicheng@qq.com>
2016-11-21 18:50:15 +0800

    add np

--- src/main/java/org/wanshicheng/ln/np/InterfaceLister.java
+++ src/main/java/org/wanshicheng/ln/np/InterfaceLister.java
@@ -6,8 +6,7 @@ import java.util.*;
 public class InterfaceLister {
 
   public static void main(String[] args) throws SocketException {
-    Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetwork
-    Interfaces();
+    Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
     while (interfaces.hasMoreElements()) {
       NetworkInterface ni = interfaces.nextElement();
       System.out.println(ni);               
